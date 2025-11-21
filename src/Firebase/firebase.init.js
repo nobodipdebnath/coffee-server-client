5e8c930c12264727bce81a4afb5c754fdbb44bf7
Nobodip Debnath <nobodipdebnath45@gmail.com>
2025-11-21 13:33:18 +0600

    add file

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCAoi2e7HoE0v7hWJ5PbkEJW3jm4kNTE2k",
-  authDomain: "coffee-store-88bdf.firebaseapp.com",
-  projectId: "coffee-store-88bdf",
-  storageBucket: "coffee-store-88bdf.firebasestorage.app",
-  messagingSenderId: "1079202730312",
-  appId: "1:1079202730312:web:4d37c6061feb492d69637d"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
